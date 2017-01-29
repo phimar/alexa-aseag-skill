e60c82bba1b45ae5ed101c472a903d6cdcdacda0
Philip Martzok <philip@holobuilder.com>
2017-01-29 21:24:52 +0100

    * Added Tests for aseag-api.js * Fixed 'toNow' value.

--- src/aseag-api.js
+++ src/aseag-api.js
@@ -1,28 +1,26 @@
 var request = require('request');
 var moment = require('moment');
 
-
 var BASE_URL = "http://ifa.aseag.de/index.php?id=344&eID=mbient_ifa"
 
 exports.getTimetableForStage = function(stageId, limit, locale, callback) {
     request.get(BASE_URL + '&res=timetable&stage=' + stageId, function(err, response, body) {
-        if (err || response.status != 200) {
-            callback("Request failed.");
+        if (err || response.statusCode != 200) {
+            callback(err || new Error("Request finished with status " + response.status));
         } else {
             var departures = [];
             try {
                 departures = JSON.parse(body);
             } catch (ignored) {}
 
-            var limited = departures.slice(0, limit - 1);
-
-            moment.locale(locale);
+            var limited = departures.slice(0, limit)
 
             var sanitized = limited.map(function(departure) {
               return {
                 'route': departure.route.name,
                 'destination': departure.departureStage.name,
-                'fromNow': moment(departure.expectedTime).toNow()
+                'toNow': moment.parseZone(departure.expectedTime).locale(locale).fromNow(),
+                'realtime': !!departure.realtime
               };
             });
             callback(null, sanitized);
---
+++ src/test/test.js
@@ -0,0 +1,35 @@
+var chai = require('chai');
+chai.use(require('chai-string'));
+var expect = chai.expect;
+
+var aseag = require('../aseag-api');
+
+describe('aseag-api', function() {
+  describe('#getTimetableForStage()', function() {
+    it('should not return an error for a valid stageId', function(done) {
+      // Stage 253 is Kuckelkorn
+      aseag.getTimetableForStage(253, 5, "en_GB", done);
+    });
+    it('should return an array with expected length and format for a valid stageId', function(done) {
+      var LIMIT = 5;
+      // Stage 253 is Kuckelkorn
+      aseag.getTimetableForStage(253, LIMIT, "en_GB", function(err, departures) {
+        if(err) {
+          done(err);
+        } else {
+          console.log(departures);
+          expect(departures).to.be.instanceof(Array);
+          expect(departures).to.have.length.within(0,LIMIT);
+          departures.forEach(function(departure) {
+            expect(departure).to.have.property('route');
+            expect(departure).to.have.property('destination');
+            expect(departure).to.have.property('toNow');
+            expect(departure.toNow).to.startsWith('in');
+            expect(departure.toNow).to.contain('minute');
+          });
+          done();
+        }
+      });
+    });
+  });
+});
